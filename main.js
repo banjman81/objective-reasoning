dad8b1837d5c3df625eb8cb1ae2f67ac95782e75
[banjman81] <[benjamin.man97@gmail.com]>
2021-06-21 11:34:03 -0500

    Assigment commit

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(user){
+    if(user.userRole === "ADMIN"){
+        return true
+    }
+    else{
+        return false
+    }
+}
 
+function getEmail(user){
+    user.email = `${user.firstName.toLowerCase()}.${user.lastName.toLowerCase()}@codeimmersives.com`
+    return user.email
+}
+
+function getPlaylistLength(playlist){
+    return playlist.songs.length
+}
+
+function getHardestHomework(homeworks){
+    if(homeworks.length == 0){
+        return ""
+    }
+    let hardest = homeworks[0].name
+    let score = homeworks[0].averageScore
+    for(const homework of homeworks){
+        if(homework.averageScore < score){
+            hardest = homework.name
+        }
+    }
+    return hardest
+}
+
+function createPhonebook(namesInput, numbersInput){
+    let phoneBook = {}
+    if(namesInput.length == 0 && numbersInput.length == 0){
+        return phoneBook
+    }
+    for (let i=0; i< namesInput.length; i++){
+        const nameSet = namesInput[i]
+        phoneBook[nameSet] = numbersInput[i]
+    }
+    return phoneBook
+}
 
 
 // ┌─────────────────────────────────────┐
